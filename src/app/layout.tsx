521ec154ba5aa18ee2bffe9ee75b699feda1d67a
Jitendra Meena <19606386+jitu-git@users.noreply.github.com>
2025-09-26 17:16:31 +0530

    add google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-PQ7LDRR8T0" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
